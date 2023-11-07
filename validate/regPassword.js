5a86c5985fd35ebd914ef0ff855cead2087c498f
“mengyingqi” <mengyingqi_XX@163.com>
2023-11-07 17:31:07 +0800

    feat: 密码校验

---
+++ validate/regPassword.js
@@ -0,0 +1,23 @@
+export function regPassword(password) {
+  if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{10,20}$/.test(password)) {
+    // 必须包含大写、小写、数字，并且长度在10-20
+    return true
+  } else if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*[\p{P}]).{10,20}$/.test(password)) {
+    // 必须包含大写、小写、标点符号，并且长度在10-20
+    return true
+  } else if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*[\W_]).{10,20}$/.test(password)) {
+    // 必须包含大写、小写、特殊符号，并且长度在10-
+    return true
+  } else if (/^(?=.*[a-z])(?=.*\d)(?=.*[\p{P}]).{10,20}$/.test(password)) {
+    // 必须包含小写、数字、标点符号，并且长度在10-20
+    return
+  } else if (/^(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{10,20}$/.test(password)) {
+    // 必须包含小写、数字、特殊符号，并且长度在10-20
+    return true
+  } else if (/^(?=.*\d)(?=.*[\W_]).{10,20}$/.test(password)) {
+    // 必须包含数字、特殊符号、标点符号，并且长度在10-20
+    return true
+  } else {
+    return false
+  }
+}
